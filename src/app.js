88cdc3a492ddf7c1da1971cbbec62769aec02731
임은성 <strongbone@naver.com>
2025-05-27 22:01:39 +0900

    node.js express.js 를 통한 서버환경 구축

---
+++ src/app.js
@@ -0,0 +1,50 @@
+import Content from "./components/Content.js";
+import TabBar from "./components/Tabbar.js";
+import { request } from "./components/api.js";
+
+export default function App($app) {
+
+    this.state = {
+        currentTab: window.location.pathname.replace("/",'')||'all',
+        photos: [],
+    };
+
+    const tab = new TabBar({
+        $app,
+        initialState: '',
+        onClick: async (name)=> {
+            history.pushState(null, `${name} 사진`, name);
+            this.updateContent(name);
+        },
+    });
+    const content = new Content({
+        $app,
+        initialState: [],
+    });
+
+    this.setState = (newState)=>{
+        this.state = newState;
+        tab.setState(this.state.currentTab);
+        content.setState(this.state.photos);
+    };
+    this.updateContent = async(tabName)=>{
+        try{
+            const currentTab = tabName === 'all'?'':tabName;
+            const photos = await request(currentTab);
+            this.setState({
+            ...this.state,
+            currentTab : tabName,
+            photos: photos,
+        });
+        }catch(err){
+            console.log(err);
+        }
+    }
+    window.addEventListener('popstate', async () => {
+        this.updateContent(window.location.pathname.replace('/',''));
+    });
+    const init = async ()=>{
+        this.updateContent(this.state.currentTab);
+    };
+    init();
+}
